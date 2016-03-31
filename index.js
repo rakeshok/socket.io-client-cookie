e010ecbd47bfa8d4c373cc523b617febfd2ed750
Matt Lavin <matt.lavin@inin.com>
2016-03-31 11:10:06 -0400

    Support engine.io-client versions > 1.6

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-// XMLHttpRequest to override.
-var xhrPath = '../socket.io-client/node_modules/engine.io-client/node_modules/xmlhttprequest';
-
-//Require it for the first time to store it in the require.cache
-require(xhrPath);
-
-//Get the resolved filename which happens to be the key of the module in the cache object
-var xhrName = require.resolve(xhrPath);
-
-//Get the cached xhr module
-var cachedXhr = require.cache[xhrName].exports;
+var debug = require('debug')('socket.io-client-cookies');
 
 //Cookies to be applied in the xhr
 var cookies;
 
-////Monkey Patch
-var newXhr = function () {
-    cachedXhr.apply(this, arguments);
-    this.setDisableHeaderCheck(true);
+function patchXHR(xhrPath) {
+    //Require it for the first time to store it in the require.cache
+    require(xhrPath);
+    
+    //Get the resolved filename which happens to be the key of the module in the cache object
+    var xhrName = require.resolve(xhrPath);
+    
+    //Get the cached xhr module
+    var cachedXhr = require.cache[xhrName].exports;
+    
+    ////Monkey Patch
+    var newXhr = function () {
+        cachedXhr.apply(this, arguments);
+        this.setDisableHeaderCheck(true);
+        
+        var stdOpen = this.open;
+        this.open = function () {
+            stdOpen.apply(this, arguments);
+            this.setRequestHeader('Cookie', cookies);
+        }
+    };
+    
+    newXhr.XMLHttpRequest = newXhr;
+    require.cache[xhrName].exports = newXhr;
+}
 
-    var stdOpen = this.open;
-    this.open = function () {
-        stdOpen.apply(this, arguments);
-        this.setRequestHeader('Cookie', cookies);
+try {
+    patchXHR('../socket.io-client/node_modules/engine.io-client/node_modules/xmlhttprequest');
+} catch (e) {
+    debug('Failed to patch engine.io v1.5.x.  If you are running a later version you can ignore this', e);
+}
 
-    }
-};
+try {
+    patchXHR('../socket.io-client/node_modules/engine.io-client/lib/xmlhttprequest');
+} catch (e) {
+    debug('Failed to patch engine.io v1.6.x.  If you are running an earlier version you can ignore this', e);
+}
 
-newXhr.XMLHttpRequest = newXhr;
-require.cache[xhrName].exports = newXhr;
-module.exports = newXhr;
+try {
+    patchXHR('../socket.io-client/node_modules/engine.io-client/node_modules/xmlhttprequest-ssl');
+} catch (e) {
+    debug('Failed to patch engine.io v1.6.x.  If you are running an earlier version you can ignore this', e);
+}
 
 module.exports.setCookies = function(newCookies) {
     cookies = newCookies;
